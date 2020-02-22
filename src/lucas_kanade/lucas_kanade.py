3c1d5793566a6f1c07d43b48f4e2727b92263cde
Daniel Hammer <braineniac@protonmail.com>
2020-02-22 16:58:49 +0100

    Basic optical flow estimation

--- src/lucas_kanade/lucas_kanade.py
+++ src/lucas_kanade/lucas_kanade.py
@@ -9,51 +9,68 @@ def calcOpticalFlowPyrLK(prevImg, nextImg,
                          status=[], er=[],
                          winSize=(3, 3), maxLevel=5,
                          criteria=[], flags=[], minEigThreshold=0):
-    # get spacial derivatives
+    # check window size
+    if winSize[0] % 2 != 1 or winSize[1] % 2 != 1:
+        print("winSize must be an odd number!")
+        exit(-1)
+
+    # get derivatives
     Ix = cv2.Sobel(prevImg, -1, 1, 0)
     Iy = cv2.Sobel(prevImg, -1, 0, 1)
-
     It = np.abs(prevImg, nextImg)
 
-    # create window and fill it
-    Ix_win = np.zeros(winSize[0] * winSize[1])
-    Iy_win = np.zeros(winSize[0] * winSize[1])
-    It_win = np.zeros(winSize[0] * winSize[1])
+    # create neighborhood vecotors
+    Ix_v = np.zeros(winSize[0] * winSize[1])
+    Iy_v = np.zeros(winSize[0] * winSize[1])
+    It_v = np.zeros(winSize[0] * winSize[1])
 
-    for point in prevPts:
+    st = np.zeros((prevPts.shape[0], 1), dtype=bool)  # status
+    nextPts = np.zeros(prevPts.shape, dtype=int)
+    err = []
 
-        # remove outer list
-        point = point[0]
-
-        x, y = point
+    for i in range(0, prevPts.shape[0]):
+        prevPt = prevPts[i][0]  # remove outer list
+        x, y = prevPt
         x, y = int(x), int(y)
 
-        # copy neighborhood points to windows
         dx_start = winSize[1]//2
         dy_start = winSize[0]//2
         dx_end = (winSize[1]//2) + 1
         dy_end = (winSize[0]//2) + 1
 
-        Ix_win = Ix[
-            (x-dx_start):(x+dx_end),
-            (y-dy_start):(y+dy_end)].reshape((winSize[0] * winSize[1], 1))
-        Iy_win = Iy[
-            (x-dx_start):(x+dx_end),
-            (y-dy_start):(y+dy_end)].reshape((winSize[0] * winSize[1], 1))
-        It_win = It[
-            (x-dx_start):(x+dx_end),
-            (y-dy_start):(y+dy_end)].reshape((winSize[0] * winSize[1], 1))
+        try:
+            # copy neighborhood points to column vectors
+            Ix_v = Ix[
+                (x-dx_start):(x+dx_end),
+                (y-dy_start):(y+dy_end)].reshape((winSize[0] * winSize[1], 1))
+            Iy_v = Iy[
+                (x-dx_start):(x+dx_end),
+                (y-dy_start):(y+dy_end)].reshape((winSize[0] * winSize[1], 1))
+            It_v = It[
+                (x-dx_start):(x+dx_end),
+                (y-dy_start):(y+dy_end)].reshape((winSize[0] * winSize[1], 1))
 
-        next_points = calcNextPts(Ix_win, Iy_win, It_win)
-        print(next_points)
+            uv = calcNextPts(Ix_v, Iy_v, It_v).reshape((1, 2))
+            nextPt = prevPt + uv
+            nextPts[i][0][0] = np.rint(nextPt[0][0])
+            nextPts[i][0][1] = np.rint(nextPt[0][1])
+            st[i] = True  # point is good
 
-        exit(0)
+        except ValueError:
+            # in this case there is no neighborhood, skipping point
+            pass
+
+    # print(nextPts)
+    # print(prevPts)
+    # print(st)
+    #print(nextPts)
+    return nextPts, st, err
 
 
-def calcNextPts(Ix_win, Iy_win, It_win):
+def calcNextPts(Ix_v, Iy_v, It_v):
     # computes with least squares method y=Sp
-    y = np.array(-It_win)
-    S = np.concatenate([Ix_win, Iy_win], axis=1)
+    y = np.array(-It_v)
+    S = np.concatenate([Ix_v, Iy_v], axis=1)
     x, res, rank, s = np.linalg.lstsq(S, y)
     return x
 
